24473359049ebdfefafc5274ec1d324cdfe59f4e
SAM-precision-github <154251105+SAM-precision-github@users.noreply.github.com>
2024-01-02 13:53:20 +0100

    functional gltf website

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__,
+            static_url_path='', 
+            )
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+from flask import send_from_directory
+
+@app.route('/static/*')
+def send_report(path):
+    return send_from_directory('reports', path)
+
+if __name__ == '__main__':
+   app.run(debug=True)
